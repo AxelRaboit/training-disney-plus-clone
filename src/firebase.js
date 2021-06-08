829df3b12d3f18370c96fb38d405b4c0d681d5f1
Axel Raboit <axel.raboit@gmail.com>
2021-06-08 13:27:30 +0200

    fix

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
   // Your web app's Firebase configuration
-  var firebaseConfig = {
+  const firebaseConfig = {
     apiKey: "AIzaSyDB8FgInqDDs84O51Qnb0vgJnMTM2ZORbs",
     authDomain: "disneyplus-clone-375e8.firebaseapp.com",
     projectId: "disneyplus-clone-375e8",
     storageBucket: "disneyplus-clone-375e8.appspot.com",
     messagingSenderId: "1007346078408",
-    appId: "1:1007346078408:web:9dcb5ea54f34c405a78bfd"
+    appId: "1:1007346078408:web:87ae10b75e97d912a78bfd"
   };
 
   const firebaseApp = firebase.initializeApp(firebaseConfig)
